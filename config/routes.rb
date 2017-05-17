c543111bb201214430078b6cb4d480e046f3fe29
Louise Mortelette <louizemortelette@gmail.com>
2017-05-17 15:23:02 +0200

    add restaurants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+
+  get 'restaurants/create'
+
+  get 'restaurants/new'
+
+  get 'restaurants/edit'
+
+  get 'restaurants/show'
+
+  get 'restaurants/update'
+
+  get 'restaurants/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
